aaa42a853504d664f8bea18692caef44e7f27be4
HalierC <115671323+haliercesr@users.noreply.github.com>
2023-12-26 14:31:20 -0300

    Update db.js

--- Backend/src/db.js
+++ Backend/src/db.js
@@ -2,9 +2,9 @@ require("dotenv").config();
 const { Sequelize } = require("sequelize");
 const fs = require("fs");
 const path = require("path");
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_DATABASE,ss } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_DATABASE,SSLMODE } = process.env;
 const sequelize = new Sequelize(
-  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_DATABASE}??ssl=require`,
+  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_DATABASE}?SSLMODE`,
   { logging: false, native: false }
 );
 const basename = path.basename(__filename);
